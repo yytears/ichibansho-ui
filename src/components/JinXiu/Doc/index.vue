570cd64e96a82ffbe60c8c6ccedb1b456b0a15d1
zhaodaping <181916882@qq.com>
2021-06-01 09:42:55 +0800

    Initial commit

---
+++ src/components/JinXiu/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JinXiuDoc',
+  data() {
+    return {
+      url: 'http://doc.jinxiuyun.com/jinxiu-vue'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
